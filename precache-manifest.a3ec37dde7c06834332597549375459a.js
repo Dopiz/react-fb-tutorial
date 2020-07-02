6a231833f52af3678fbc8025b8aa7f397374faca
Dopiz Liu <dopiz.liu@jkos.com>
2020-07-02 14:43:18 +0800

    Updates

---
+++ precache-manifest.a3ec37dde7c06834332597549375459a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff1888cd8e8029d39d97b1d1a9bb56a",
+    "url": "/react-fb-tutorial/index.html"
+  },
+  {
+    "revision": "27b72cf0c24cfbccd147",
+    "url": "/react-fb-tutorial/static/css/main.a4bd9c8d.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/react-fb-tutorial/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fb-tutorial/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b72cf0c24cfbccd147",
+    "url": "/react-fb-tutorial/static/js/main.0279ad68.chunk.js"
+  },
+  {
+    "revision": "732af48caf9d5a1e75bd",
+    "url": "/react-fb-tutorial/static/js/runtime-main.485c06bd.js"
+  }
+]);
